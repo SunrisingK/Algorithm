094d7973f64bc4a2659dc05d6d4445afcb75e051
SunrisingK <sunrising_k@icloud.com>
2025-11-04 12:39:01 +0800

    把Disjoint类单独出来

--- DataStructure/DisjointSet/FindPath/code/cpp/main.cc
+++ DataStructure/DisjointSet/FindPath/code/cpp/main.cc
@@ -1,44 +1,6 @@
 #include <iostream>
-#include <vector>
+#include "../../../DisjointSet.h"
 using namespace std;
-
-class DisjointSet {
-public:
-    DisjointSet(int n) {
-        p.resize(1 + n);
-        for (int i = 1; i <= n; ++i) {
-            p[i] = i;
-        }
-        count.resize(1 + n, 1);
-    }
-
-    int find(int x) {
-        if (x != p[x]) {
-            p[x] = find(p[x]);  // 路径压缩
-        }
-        return p[x];
-    }
-
-    // 快速判断两个点 x 和 y 是否在同一个集合里
-    bool isInSame(int x, int y) {
-        return find(x) == find(y);
-    }    
-
-    // 把两个点 x 和 y 加入到同一个集合里
-    void merge(int x, int y) {
-        int px = find(x), py = find(y);
-        if (px != py) {
-            if (count[px] < count[py]) {
-                swap(px, py);
-            }
-            count[px] += count[py];
-            p[py] = px;
-        }
-    }
-private:
-    vector<int> p;          // 节点所在集合的根节点
-    vector<int> count;      // 节点所在集合的元素数量
-};
 
 
 int main(int argc, char const* argv[]) {
